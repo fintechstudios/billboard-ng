10bbfd07f7096ecf1c3fc8ec41d80b4c0f65b858
Dylan Praul <dylan.praul@gmail.com>
2017-11-13 16:35:03 -0500

    1.0.0

---
+++ dist/billboard-ng.js
@@ -0,0 +1,180 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("angular"), require("billboard.js"));
+	else if(typeof define === 'function' && define.amd)
+		define(["angular", "billboard.js"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("angular"), require("billboard.js")) : factory(root["angular"], root["billboard.js"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function(__WEBPACK_EXTERNAL_MODULE_1__, __WEBPACK_EXTERNAL_MODULE_2__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _angular = __webpack_require__(1);
+
+var _angular2 = _interopRequireDefault(_angular);
+
+var _billboard = __webpack_require__(2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var BillboardController = function () {
+  BillboardController.$inject = ['$element', '$timeout'];
+
+  _createClass(BillboardController, null, [{
+    key: '$inject',
+    get: function get() {
+      return ['$element', '$timeout'];
+    }
+  }]);
+
+  function BillboardController($element, $timeout) {
+    _classCallCheck(this, BillboardController);
+
+    this.$element = $element;
+    this.$timeout = $timeout;
+  }
+
+  _createClass(BillboardController, [{
+    key: '_generate',
+    value: function _generate() {
+      var _this = this;
+
+      if (this.chart) {
+        this.chart.destroy();
+      }
+
+      this.$timeout(function () {
+        // timeout ensures element is sized in DOM before initializing bb
+        _this.chart = _billboard.bb.generate(_extends({ bindto: _this.$element[0] }, _this.options));
+      });
+    }
+  }, {
+    key: '$onChanges',
+    value: function $onChanges(changes) {
+      if (changes.options && this.chart) {
+        this._generate();
+      }
+    }
+  }, {
+    key: '$postLink',
+    value: function $postLink() {
+      this._generate();
+    }
+  }, {
+    key: '$onDestroy',
+    value: function $onDestroy() {
+      if (this.chart) {
+        this.chart.destroy();
+      }
+    }
+  }]);
+
+  return BillboardController;
+}();
+
+_angular2.default.module('billboard-ng', []).component('billboard', {
+  controller: BillboardController,
+  bindings: {
+    options: '<'
+  }
+});
+
+exports.default = 'billboard-ng';
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_2__;
+
+/***/ })
+/******/ ]);
+});
+//# sourceMappingURL=billboard-ng.js.map
